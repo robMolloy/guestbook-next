cf95848b4cda39f163bd1404ceab85aa3ba9fbf6
robMolloy <robmolloy@hotmail.co.uk>
2024-07-08 23:37:51 +0100

    tailor nutab to fit photobook

--- src/modules/layout/Layout.tsx
+++ src/modules/layout/Layout.tsx
@@ -1,22 +1,85 @@
-import { useState } from "react";
-import { NavBar, SideMenu } from ".";
+import { NavBar } from "./NavBar";
 
-export type TLayoutProps = {
-  children: React.ReactNode;
+export type TPageLink = {
+  label: string;
+  href: string;
+  alwaysShow?: true;
+  horizontalClassName?: string;
 };
 
-export const Layout = (p: TLayoutProps) => {
-  const [showSideMenu, setShowSideMenu] = useState(true);
+const CloseDrawerWrapper: React.FC<{ children?: React.ReactNode }> = (p) => {
+  return (
+    <label
+      htmlFor="sidebar"
+      aria-label="close sidebar"
+      className="drawer-overlay"
+    >
+      {p.children}
+    </label>
+  );
+};
+const OpenDrawerWrapper: React.FC<{ children?: React.ReactNode }> = (p) => {
+  return (
+    <label
+      htmlFor="sidebar"
+      aria-label="open sidebar"
+      className="btn btn-square btn-ghost"
+    >
+      {p.children}
+    </label>
+  );
+};
 
+const NavBarContainer = (p: { children: React.ReactNode }) => {
+  return (
+    <div className="sticky top-0 z-[10]">
+      <div className="navbar w-full border-b bg-base-300">{p.children}</div>
+    </div>
+  );
+};
+const DrawerContainer = (p: { children: React.ReactNode }) => {
+  return (
+    <div className="m-0 min-h-full min-w-80 border-r bg-base-100 p-1">
+      {p.children}
+    </div>
+  );
+};
+
+const ContainerWithSpotlightBackgroundTop = (p: {
+  children: React.ReactNode;
+}) => {
+  return (
+    <div className="relative min-h-[50vh]">
+      <div className="absolute top-0 z-[-1] min-h-[90vh] min-w-full bg-gradient-to-tr from-base-100 via-base-100 via-75% to-primary sm:via-65%"></div>
+      {p.children}
+    </div>
+  );
+};
+
+export const Layout = (p: { children: React.ReactNode }) => {
   return (
     <>
-      <div className="flex">
-        <SideMenu show={showSideMenu} />
-        <div className="flex-1 max-h-screen overflow-scroll">
-          <NavBar onToggleClick={() => setShowSideMenu(!showSideMenu)} />
-          <main className={`px-8 pt-2`}>{p.children}</main>
+      <div className="drawer">
+        <input id="sidebar" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content flex flex-col">
+          <NavBarContainer>
+            <NavBar OpenDrawerWrapper={OpenDrawerWrapper}>
+              <div>asd</div>
+            </NavBar>
+          </NavBarContainer>
+          <ContainerWithSpotlightBackgroundTop>
+            {p.children}
+          </ContainerWithSpotlightBackgroundTop>
+        </div>
+        <div className="drawer-side z-[11]">
+          <CloseDrawerWrapper />
+
+          <DrawerContainer>
+            <CloseDrawerWrapper>asd </CloseDrawerWrapper>
+          </DrawerContainer>
         </div>
       </div>
+      {/* <Footer /> */}
     </>
   );
 };
--- src/modules/layout/NavBar.tsx
+++ src/modules/layout/NavBar.tsx
@@ -1,45 +1,44 @@
-import { ThemeSelector } from "../themeSelector";
+import Link from "next/link";
+import React from "react";
 
-export const NavBar = (p: { onToggleClick: () => void }) => {
+const BurgerMenuIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    className="inline-block h-6 w-6 stroke-current"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      d="M4 6h16M4 12h16M4 18h16"
+    ></path>
+  </svg>
+);
+
+export type TNavbarProps = {
+  OpenDrawerWrapper: React.FC<{ children: React.ReactNode }>;
+  children: React.ReactNode;
+};
+
+export const NavBar = (p: TNavbarProps) => {
   return (
-    <div className="navbar bg-base-200 sticky top-0 border-b z-[99]">
-      <div className="flex-none">
-        <button className="btn btn-square btn-ghost" onClick={p.onToggleClick}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            className="inline-block w-5 h-5 stroke-current"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M4 6h16M4 12h16M4 18h16"
-            ></path>
-          </svg>
-        </button>
-      </div>
-      <div className="hidden md:block">
-        <a className="btn btn-ghost text-xl">NuTab</a>
-      </div>
-      <div className="flex-1" />
-      <div className="flex gap-4 pr-4">
-        <div className="btn btn-ghost">hello</div>
-        <div className="btn btn-ghost">world</div>
+    <div className="flex w-full flex-col">
+      <div className="flex w-full">
+        <div className="block sm:hidden">
+          <p.OpenDrawerWrapper>
+            <BurgerMenuIcon />
+          </p.OpenDrawerWrapper>
+        </div>
 
-        <div className="dropdown dropdown-end dropdown-bottom">
-          <div tabIndex={0} role="button" className="btn btn-ghost">
-            <div>Themes &#x25BC;</div>
-          </div>
-          <div
-            tabIndex={0}
-            className="dropdown-content mt-1 z-[1] p-2 shadow bg-neutral"
-          >
-            <div className="h-[75vh] overflow-y-scroll">
-              <ThemeSelector />
-            </div>
-          </div>
+        <div className="flex-1">
+          <Link href="/" className="btn btn-ghost text-xl">
+            Photobook
+          </Link>
+        </div>
+        <div className="flex gap-4">
+          <div>{p.children}</div>
         </div>
       </div>
     </div>
