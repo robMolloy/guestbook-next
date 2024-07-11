2ffcee92e1ee28130b87157ea5c3dd89f8936572
robMolloy <robmolloy@hotmail.co.uk>
2024-07-12 00:54:01 +0100

    setup for firebase emulator

--- src/config/firebaseConfig.ts
+++ src/config/firebaseConfig.ts
@@ -1,8 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
-import { getAuth } from "firebase/auth";
+import { connectFirestoreEmulator, getFirestore } from "firebase/firestore";
+import { connectStorageEmulator, getStorage } from "firebase/storage";
+import { connectAuthEmulator, getAuth } from "firebase/auth";
 
 export const app = initializeApp({
   apiKey: "AIzaSyBov3LGp6m4JerjgJ_iUxMTErH3RFb70h8",
@@ -15,3 +15,9 @@ export const app = initializeApp({
 export const db = getFirestore(app);
 export const auth = getAuth();
 export const storage = getStorage(app);
+
+if (process.env.NEXT_PUBLIC_USE_FIREBASE_EMULATOR === "true") {
+  connectFirestoreEmulator(db, "127.0.0.1", 8080);
+  connectAuthEmulator(auth, "http://127.0.0.1:9099");
+  connectStorageEmulator(storage, "127.0.0.1", 9199);
+}
