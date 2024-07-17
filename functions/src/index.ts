b08d3e67b325956a991a93b8a34fc83266bd746c
robMolloy <robmolloy@hotmail.co.uk>
2024-07-17 15:49:19 +0100

    practice  functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,13 +2,14 @@
  * Import function triggers from their respective submodules:
  *
  * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
  *
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
 import { onRequest } from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
+// import * as storage from "firebase-functions/v2/firestore";
+import * as storage from "firebase-functions/v2/storage";
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
@@ -17,3 +18,5 @@ export const helloWorld = onRequest((request, response) => {
   logger.info("Hello logs!", { structuredData: true });
   response.send("Hello from cat!");
 });
+
+export const newFn = storage.onObjectFinalized("print-queue-images", (storage) => {});
