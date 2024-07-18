7cd0c2c1c87c28fb1ad596cb12a1febaa7614e86
robMolloy <robmolloy@hotmail.co.uk>
2024-07-18 14:16:10 +0100

    minor useFlow updates

--- src/modules/cycleFlow/useFlow.ts
+++ src/modules/cycleFlow/useFlow.ts
@@ -1,11 +1,14 @@
 import { useSignal } from "@/utils/useSignal";
 import { useState } from "react";
+import { v4 as uuid } from "uuid";
 
 type TFlowStatus = "ready" | "capturing" | "selecting" | "sending" | "fail" | "success";
 
 export const useFlow = () => {
   const [selectableImageDataUrls, setSelectableImageDataUrls] = useState<string[]>([]);
   const [selectedImageDataUrl, setSelectedImageDataUrl] = useState<string | undefined>();
+  const [status, setStatus] = useState<TFlowStatus>("ready");
+  const [groupId, setGroupId] = useState<string>(uuid());
 
   const addSelectableImageDataUrl = (imageDataUrl: string) => {
     if (selectableImageDataUrls.length >= 4) return;
@@ -20,17 +23,17 @@ export const useFlow = () => {
     captureSignal.changeSignal();
   };
 
-  const [status, setStatus] = useState<TFlowStatus>("ready");
-
   const reset = () => {
     setStatus("ready");
     setSelectableImageDataUrls([]);
     setSelectedImageDataUrl(undefined);
+    setGroupId(uuid());
   };
 
   return {
     status,
     setStatus,
+    groupId,
     selectableImageDataUrls,
     setSelectableImageDataUrls,
     addSelectableImageDataUrl,
