fd34e5f1710ced73b7a312a3648ded2eda280823
robMolloy <robmolloy@hotmail.co.uk>
2024-07-19 09:57:31 +0100

    remove unnecessary nav/drawer components + almost working show/hide navbar

--- src/modules/layout/Layout.tsx
+++ src/modules/layout/Layout.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { NavBar } from "./NavBar";
 
 export type TPageLink = {
@@ -7,47 +8,15 @@ export type TPageLink = {
   horizontalClassName?: string;
 };
 
-const CloseDrawerWrapper: React.FC<{ children?: React.ReactNode }> = (p) => {
-  return (
-    <label
-      htmlFor="sidebar"
-      aria-label="close sidebar"
-      className="drawer-overlay"
-    >
-      {p.children}
-    </label>
-  );
-};
-const OpenDrawerWrapper: React.FC<{ children?: React.ReactNode }> = (p) => {
-  return (
-    <label
-      htmlFor="sidebar"
-      aria-label="open sidebar"
-      className="btn btn-square btn-ghost"
-    >
-      {p.children}
-    </label>
-  );
-};
-
 const NavBarContainer = (p: { children: React.ReactNode }) => {
   return (
-    <div className="sticky top-0 z-[10] " style={{ display: "none" }}>
+    <div className="sticky top-0 z-[10]">
       <div className="navbar w-full border-b bg-base-300">{p.children}</div>
     </div>
   );
 };
-const DrawerContainer = (p: { children: React.ReactNode }) => {
-  return (
-    <div className="m-0 min-h-full min-w-80 border-r bg-base-100 p-1">
-      {p.children}
-    </div>
-  );
-};
 
-const ContainerWithSpotlightBackgroundTop = (p: {
-  children: React.ReactNode;
-}) => {
+const ContainerWithSpotlightBackgroundTop = (p: { children: React.ReactNode }) => {
   return (
     <div className="relative min-h-[50vh]">
       <div className="absolute top-0 z-[-1] min-h-[90vh] min-w-full bg-gradient-to-tr from-base-100 via-base-100 via-75% to-primary sm:via-65%"></div>
@@ -57,29 +26,47 @@ const ContainerWithSpotlightBackgroundTop = (p: {
 };
 
 export const Layout = (p: { children: React.ReactNode }) => {
+  const [showNavStatus, setShowNavStatus] = useState<"show" | "hide" | "inform">("show");
+  const [clickHoldTimer, setClickHoldTimer] = useState<NodeJS.Timeout>();
+
+  useEffect(() => {
+    console.log(`Layout.tsx:${/*LL*/ 33}`, { showNavStatus });
+    if (showNavStatus === "inform") {
+      setTimeout(() => {
+        console.log(`Layout.tsx:${/*LL*/ 35}`, { showNavStatus });
+        setShowNavStatus((x) => (showNavStatus === "inform" ? "hide" : x));
+      }, 5000);
+    }
+  }, [showNavStatus]);
+
   return (
     <>
-      <div className="drawer">
-        <input id="sidebar" type="checkbox" className="drawer-toggle" />
-        <div className="drawer-content flex flex-col ">
-          <NavBarContainer>
-            <NavBar OpenDrawerWrapper={OpenDrawerWrapper}>
-              <div>asd</div>
-            </NavBar>
-          </NavBarContainer>
-          <ContainerWithSpotlightBackgroundTop>
-            {p.children}
-          </ContainerWithSpotlightBackgroundTop>
+      {showNavStatus === "show" && (
+        <NavBarContainer>
+          <NavBar>
+            <div className="flex gap-4">
+              <button className="btn btn-outline" onClick={() => setShowNavStatus("inform")}>
+                Hide Nav
+              </button>
+            </div>
+          </NavBar>
+        </NavBarContainer>
+      )}
+      {showNavStatus !== "show" && (
+        <div
+          className="absolute left-0 right-0 z-50"
+          onMouseDown={() => setClickHoldTimer(setTimeout(() => setShowNavStatus("show"), 2000))}
+          onMouseUp={() => clearTimeout(clickHoldTimer)}
+        >
+          {showNavStatus === "inform" && (
+            <div className="border text-center">
+              click and hold here for 3 seconds to show nav bar
+            </div>
+          )}
+          {showNavStatus === "hide" && <br />}
         </div>
-        <div className="drawer-side z-[11]">
-          <CloseDrawerWrapper />
-
-          <DrawerContainer>
-            <CloseDrawerWrapper>asd </CloseDrawerWrapper>
-          </DrawerContainer>
-        </div>
-      </div>
-      {/* <Footer /> */}
+      )}
+      <ContainerWithSpotlightBackgroundTop>{p.children}</ContainerWithSpotlightBackgroundTop>
     </>
   );
 };
