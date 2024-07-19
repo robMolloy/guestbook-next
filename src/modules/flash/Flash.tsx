6f0173da67506138a281151585a99f4f01d25ddd
robMolloy <robmolloy@hotmail.co.uk>
2024-07-19 19:23:48 +0100

    flash formatting changes

--- src/modules/flash/Flash.tsx
+++ src/modules/flash/Flash.tsx
@@ -2,16 +2,12 @@ import { useSignal, useSignalListener } from "@/utils/useSignal";
 import { useEffect, useState } from "react";
 import { v4 as uuid } from "uuid";
 
-export const Flash = (p: {
-  signal: ReturnType<typeof useSignal>;
-  children?: React.ReactNode;
-}) => {
+export const Flash = (p: { signal: ReturnType<typeof useSignal>; children?: React.ReactNode }) => {
   const [flashes, setFlashes] = useState<string[]>([]);
   const [finishedFlashes, setFinishedFlashes] = useState<string[]>([]);
 
   const addFlash = () => setFlashes((prevFlashes) => [...prevFlashes, uuid()]);
-  const removeFlash = (x: string) =>
-    setFinishedFlashes([...finishedFlashes, x]);
+  const removeFlash = (x: string) => setFinishedFlashes([...finishedFlashes, x]);
 
   useSignalListener({
     signal: p.signal,
@@ -29,14 +25,14 @@ export const Flash = (p: {
 
   return (
     <>
-      <div className="relative w-full h-full z-[99]">
+      <div className="relative z-[99] h-full w-full">
         {flashes
           .filter((flash) => !finishedFlashes.includes(flash))
           .map((x) => (
             <div
               key={x}
               onAnimationEndCapture={() => removeFlash(x)}
-              className="absolute opacity-0 h-full w-full bg-white"
+              className="absolute h-full w-full bg-white opacity-0"
               style={{ animation: "flashFade 1s" }}
             />
           ))}
