0a8d2633a6f46eef7f2d9a1f32976ce2ac58bb95
robMolloy <robmolloy@hotmail.co.uk>
2024-07-20 11:38:24 +0100

    manage-event.page realtime updates

--- src/pages/[eventId].page.tsx
+++ src/pages/[eventId].page.tsx
@@ -1,31 +1,63 @@
 import { Typography } from "@/components";
+import { db } from "@/config/firebaseConfig";
 import { useAuthStore } from "@/stores/useAuthStore";
 import {
+  TSelectedImageDbEntry,
   deleteSelectedImageDbEntry,
-  readAllValidSelectedImageDbEntries,
+  selectedImageDbEntrySchema,
 } from "@/utils/firestoreUtils";
+import dayjs from "dayjs";
+import { Timestamp, and, collection, onSnapshot, orderBy, query, where } from "firebase/firestore";
 import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
+
+const convertFirestoreTimestampToFormattedDateString = (x: Timestamp) => {
+  const date = dayjs(x.seconds * 1000);
+  return !!date ? date.format("D MMM YYYY HH:mm") : undefined;
+};
 
 export default function Page() {
   const router = useRouter();
   const eventId = router.query.eventId as string;
   const authStore = useAuthStore();
   const safeAuthStore = authStore.getSafeStore();
-  const [safeSelectedImages, setSafeSelectedImages] = useState<
-    Awaited<ReturnType<typeof readAllValidSelectedImageDbEntries>> | { success: undefined }
-  >();
+  const [selectedImages, setSelectedImages] = useState<TSelectedImageDbEntry[]>([]);
   const [deletedSelectedImageIds, setDeletedSelectedImageIds] = useState<string[]>([]);
 
   useEffect(() => {
-    (async () => {
-      if (safeAuthStore.status !== "logged_in") return;
-      const selectedImagesResponse = await readAllValidSelectedImageDbEntries({
-        uid: safeAuthStore.user.uid,
-        eventId: eventId,
+    if (safeAuthStore.status !== "logged_in") return;
+    const collectionName = "selectedImages";
+    const collectionRef = collection(db, collectionName);
+    const q1 = query(
+      collectionRef,
+      and(where("uid", "==", safeAuthStore.user.uid), where("eventId", "==", eventId)),
+      orderBy("createdAt", "desc"),
+    );
+    const unsub = onSnapshot(q1, (snapshot) => {
+      const docs = snapshot.docs
+        .map((doc) => {
+          const data = doc.data();
+          return {
+            id: doc.id,
+            ...data,
+            createdAt: convertFirestoreTimestampToFormattedDateString(data?.createdAt),
+            updatedAt: convertFirestoreTimestampToFormattedDateString(data?.updatedAt),
+          };
+        })
+        .map((x) => selectedImageDbEntrySchema.safeParse(x))
+        .map((x) => (x.success ? x.data : undefined))
+        .filter((x) => !!x) as TSelectedImageDbEntry[];
+      setSelectedImages(docs);
+
+      snapshot.docChanges().forEach((change) => {
+        if (change.type === "added")
+          fetch("http://localhost:3000/api/print-image", {
+            method: "POST",
+            body: JSON.stringify({ imageUrl: change.doc.data().downloadUrl }),
+          });
       });
-      setSafeSelectedImages(selectedImagesResponse);
-    })();
+    });
+    return () => unsub();
   }, []);
 
   return (
@@ -41,68 +73,62 @@ export default function Page() {
         </button>
       </div>
       <br />
-      {safeSelectedImages?.success && safeSelectedImages.data.length === 0 && (
-        <div>No images added yet</div>
-      )}
-      {safeSelectedImages?.success &&
-        safeSelectedImages.data.map((x) => {
-          const modalId = `modal-delete-${x.id}`;
-          const isDeleted = deletedSelectedImageIds.includes(x.id);
-          return (
-            <React.Fragment key={x.id}>
-              <div tabIndex={0} className="collapse bg-neutral shadow-lg">
-                <input type="checkbox" />
-                <div className="collapse-title text-xl font-medium">
-                  {x.createdAt} {isDeleted && "[DELETED]"}
-                </div>
-                {!isDeleted && (
-                  <div className="collapse-content pt-0">
-                    <img src={x.downloadUrl} className="m-0" />
-                    <br />
-                    <button
-                      className="btn btn-info"
-                      onClick={() => {
-                        const deleteModal = document.getElementById(modalId) as
-                          | HTMLDialogElement
-                          | undefined;
-                        deleteModal?.showModal();
-                      }}
-                    >
-                      Delete
-                    </button>
-                    <dialog id={modalId} className="modal">
-                      <div className="modal-box">
-                        <p className="py-4">Are you sure you want to delete this image?</p>
-                        <form method="dialog" className="flex gap-4">
-                          <button
-                            className="btn btn-error"
-                            onClick={async () => {
-                              if (safeAuthStore.status !== "logged_in") return;
-                              const deleteResponse = await deleteSelectedImageDbEntry({
-                                id: x.id,
-                                uid: safeAuthStore.user.uid,
-                              });
+      {selectedImages.map((x) => {
+        const modalId = `modal-delete-${x.id}`;
 
-                              if (!deleteResponse.success) return;
-                              setDeletedSelectedImageIds([...deletedSelectedImageIds, x.id]);
-                            }}
-                          >
-                            Yes
-                          </button>
-                          <button className="btn btn-outline">Cancel</button>
-                        </form>
-                      </div>
-                      <form method="dialog" className="modal-backdrop">
-                        <button>close</button>
+        return (
+          <React.Fragment key={x.id}>
+            <div tabIndex={0} className="collapse bg-neutral shadow-lg">
+              <input type="checkbox" />
+              <div className="collapse-title text-xl font-medium">{x.createdAt}</div>
+              {
+                <div className="collapse-content pt-0">
+                  <img src={x.downloadUrl} className="m-0" />
+                  <br />
+                  <button
+                    className="btn btn-info"
+                    onClick={() => {
+                      const deleteModal = document.getElementById(modalId) as
+                        | HTMLDialogElement
+                        | undefined;
+                      deleteModal?.showModal();
+                    }}
+                  >
+                    Delete
+                  </button>
+                  <dialog id={modalId} className="modal">
+                    <div className="modal-box">
+                      <p className="py-4">Are you sure you want to delete this image?</p>
+                      <form method="dialog" className="flex gap-4">
+                        <button
+                          className="btn btn-error"
+                          onClick={async () => {
+                            if (safeAuthStore.status !== "logged_in") return;
+                            const deleteResponse = await deleteSelectedImageDbEntry({
+                              id: x.id,
+                              uid: safeAuthStore.user.uid,
+                            });
+
+                            if (!deleteResponse.success) return;
+                            setDeletedSelectedImageIds([...deletedSelectedImageIds, x.id]);
+                          }}
+                        >
+                          Yes
+                        </button>
+                        <button className="btn btn-outline">Cancel</button>
                       </form>
-                    </dialog>
-                  </div>
-                )}
-              </div>
-              <br />
-            </React.Fragment>
-          );
-        })}
+                    </div>
+                    <form method="dialog" className="modal-backdrop">
+                      <button>close</button>
+                    </form>
+                  </dialog>
+                </div>
+              }
+            </div>
+            <br />
+          </React.Fragment>
+        );
+      })}
     </Typography>
   );
 }
