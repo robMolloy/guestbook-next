20436be9b040bc1a3290943519b0d55d4d540f6a
robMolloy <robmolloy@hotmail.co.uk>
2024-07-21 16:21:35 +0100

    manage-event.page improve code

--- src/pages/[eventId].page.tsx
+++ src/pages/[eventId].page.tsx
@@ -1,17 +1,60 @@
 import { Typography } from "@/components";
-import { db } from "@/config/firebaseConfig";
 import { useAuthStore } from "@/stores/useAuthStore";
 import {
   TSelectedImageDbEntry,
   deleteSelectedImageDbEntry,
-  selectedImageDbEntrySchema,
+  watchValidSelectedImageDbEntries,
 } from "@/utils/firestoreUtils";
 import dayjs from "dayjs";
-import { Timestamp, and, collection, onSnapshot, orderBy, query, where } from "firebase/firestore";
 import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
 
-const convertFirestoreTimestampToFormattedDateString = (x: Timestamp) => {
+const DisplaySelectedImageModal = (p: {
+  selectedImage: TSelectedImageDbEntry;
+  onDelete: (x: TSelectedImageDbEntry) => void;
+}) => {
+  const x = p.selectedImage;
+  const modalId = `modal-delete-${x.id}`;
+  return (
+    <div tabIndex={0} className="collapse bg-neutral shadow-lg">
+      <input type="checkbox" />
+      <div className="collapse-title min-h-0 p-2 text-xl font-medium">
+        {convertFirestoreTimestampToFormattedDateString(x.createdAt)}
+      </div>
+      {
+        <div className="collapse-content pt-0">
+          <img src={x.downloadUrl} className="m-0" />
+          <br />
+          <button
+            className="btn btn-info"
+            onClick={() => {
+              const deleteModal = document.getElementById(modalId) as HTMLDialogElement | undefined;
+              deleteModal?.showModal();
+            }}
+          >
+            Delete
+          </button>
+          <dialog id={modalId} className="modal">
+            <div className="modal-box">
+              <p className="py-4">Are you sure you want to delete this image?</p>
+              <form method="dialog" className="flex gap-4">
+                <button className="btn btn-error" onClick={async () => p.onDelete(x)}>
+                  Yes
+                </button>
+                <button className="btn btn-outline">Cancel</button>
+              </form>
+            </div>
+            <form method="dialog" className="modal-backdrop">
+              <button>close</button>
+            </form>
+          </dialog>
+        </div>
+      }
+    </div>
+  );
+};
+
+const convertFirestoreTimestampToFormattedDateString = (x: { seconds: number }) => {
   const date = dayjs(x.seconds * 1000);
   return !!date ? date.format("D MMM YYYY HH:mm") : undefined;
 };
@@ -26,36 +69,17 @@ export default function Page() {
 
   useEffect(() => {
     if (safeAuthStore.status !== "logged_in") return;
-    const collectionName = "selectedImages";
-    const collectionRef = collection(db, collectionName);
-    const q1 = query(
-      collectionRef,
-      and(where("uid", "==", safeAuthStore.user.uid), where("eventId", "==", eventId)),
-      orderBy("createdAt", "desc"),
-    );
-    const unsub = onSnapshot(q1, (snapshot) => {
-      const docs = snapshot.docs
-        .map((doc) => {
-          const data = doc.data();
-          return {
-            id: doc.id,
-            ...data,
-            createdAt: convertFirestoreTimestampToFormattedDateString(data?.createdAt),
-            updatedAt: convertFirestoreTimestampToFormattedDateString(data?.updatedAt),
-          };
-        })
-        .map((x) => selectedImageDbEntrySchema.safeParse(x))
-        .map((x) => (x.success ? x.data : undefined))
-        .filter((x) => !!x) as TSelectedImageDbEntry[];
-      setSelectedImages(docs);
 
-      snapshot.docChanges().forEach((change) => {
-        if (change.type === "added")
-          fetch("http://localhost:3000/api/print-image", {
-            method: "POST",
-            body: JSON.stringify({ imageUrl: change.doc.data().downloadUrl }),
-          });
-      });
+    const unsub = watchValidSelectedImageDbEntries({
+      uid: safeAuthStore.user.uid,
+      eventId,
+      onNewSnapshot: (docs) => {
+        setSelectedImages(docs);
+      },
+      onAddedDoc: (doc) => {
+        const options = { method: "POST", body: JSON.stringify(doc) };
+        fetch("http://localhost:3000/api/print-image", options);
+      },
     });
     return () => unsub();
   }, []);
@@ -73,62 +97,26 @@ export default function Page() {
         </button>
       </div>
       <br />
-      {selectedImages.map((x) => {
-        const modalId = `modal-delete-${x.id}`;
-
-        return (
-          <React.Fragment key={x.id}>
-            <div tabIndex={0} className="collapse bg-neutral shadow-lg">
-              <input type="checkbox" />
-              <div className="collapse-title text-xl font-medium">{x.createdAt}</div>
-              {
-                <div className="collapse-content pt-0">
-                  <img src={x.downloadUrl} className="m-0" />
-                  <br />
-                  <button
-                    className="btn btn-info"
-                    onClick={() => {
-                      const deleteModal = document.getElementById(modalId) as
-                        | HTMLDialogElement
-                        | undefined;
-                      deleteModal?.showModal();
-                    }}
-                  >
-                    Delete
-                  </button>
-                  <dialog id={modalId} className="modal">
-                    <div className="modal-box">
-                      <p className="py-4">Are you sure you want to delete this image?</p>
-                      <form method="dialog" className="flex gap-4">
-                        <button
-                          className="btn btn-error"
-                          onClick={async () => {
-                            if (safeAuthStore.status !== "logged_in") return;
-                            const deleteResponse = await deleteSelectedImageDbEntry({
-                              id: x.id,
-                              uid: safeAuthStore.user.uid,
-                            });
-
-                            if (!deleteResponse.success) return;
-                            setDeletedSelectedImageIds([...deletedSelectedImageIds, x.id]);
-                          }}
-                        >
-                          Yes
-                        </button>
-                        <button className="btn btn-outline">Cancel</button>
-                      </form>
-                    </div>
-                    <form method="dialog" className="modal-backdrop">
-                      <button>close</button>
-                    </form>
-                  </dialog>
-                </div>
-              }
-            </div>
-            <br />
-          </React.Fragment>
-        );
-      })}
+      <div className="flex flex-col gap-4">
+        {selectedImages.map((x) => {
+          return (
+            <React.Fragment key={x.id}>
+              <DisplaySelectedImageModal
+                selectedImage={x}
+                onDelete={async (x) => {
+                  if (safeAuthStore.status !== "logged_in") return;
+                  const deleteResponse = await deleteSelectedImageDbEntry({
+                    id: x.id,
+                    uid: safeAuthStore.user.uid,
+                  });
+                  if (!deleteResponse.success) return;
+                  setDeletedSelectedImageIds([...deletedSelectedImageIds, x.id]);
+                }}
+              />
+            </React.Fragment>
+          );
+        })}
+      </div>
     </Typography>
   );
 }
